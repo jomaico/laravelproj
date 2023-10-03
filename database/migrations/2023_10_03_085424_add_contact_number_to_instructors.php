d2fc743e69d3d07a08b8ab3e01e486bb5caf61b9
jomaico <jomaicoabusca600@gmail.com>
2023-10-03 02:24:42 -0700

    added activity #5

---
+++ database/migrations/2023_10_03_085424_add_contact_number_to_instructors.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('instructors', function (Blueprint $table) {
+           $table->string('contact_namber', 11)->after('name');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('instructors', function (Blueprint $table) {
+            //
+        });
+    }
+};
